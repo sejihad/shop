cc436fbe5dcc93d6332399d477f1da3ad1bb1c95
Jihad Mia <153150003+sejihad@users.noreply.github.com>
2024-03-14 22:04:19 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,97 @@
+/*=============== SHOW MENU ===============*/
+const nav = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    nav.classList.add("show-menu");
+  });
+}
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    nav.classList.remove("show-menu");
+  });
+}
+/*=============== SHOW CART ===============*/
+const cart = document.getElementById("cart"),
+  cartShop = document.getElementById("cart-shop"),
+  cartClose = document.getElementById("cart-close");
+
+/*===== CART SHOW =====*/
+/* Validate if constant exists */
+if (cartShop) {
+  cartShop.addEventListener("click", () => {
+    cart.classList.add("show-cart");
+  });
+}
+/*===== CART HIDDEN =====*/
+/* Validate if constant exists */
+if (cartClose) {
+  cartClose.addEventListener("click", () => {
+    cart.classList.remove("show-cart");
+  });
+}
+/*=============== SHOW LOGIN ===============*/
+const login = document.getElementById("login"),
+  loginButton = document.getElementById("login-button"),
+  loginClose = document.getElementById("login-close");
+/*===== LOGIN SHOW =====*/
+/* Validate if constant exists */
+if (loginButton) {
+  loginButton.addEventListener("click", () => {
+    login.classList.add("show-login");
+  });
+}
+/*===== LOGIN HIDDEN =====*/
+/* Validate if constant exists */
+if (loginClose) {
+  loginClose.addEventListener("click", () => {
+    login.classList.remove("show-login");
+  });
+}
+/*=============== HOME SWIPER ===============*/
+let homeSwiper = new Swiper(".home-swiper", {
+  spaceBetween: 30,
+  loop: "true",
+  pagination: {
+    el: ".swiper-pagination",
+
+    clickable: true,
+  },
+});
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
+/*=============== NEW SWIPER ===============*/
+let newSwiper = new Swiper(".new-swiper", {
+  spaceBetween: 16,
+  slidesPerView: "auto",
+  centeredSlides: true,
+  loop: "true",
+});
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+/*=============== LIGHT BOX ===============*/
+
+/*=============== QUESTIONS ACCORDION ===============*/
+
+/*=============== STYLE SWITCHER ===============*/
